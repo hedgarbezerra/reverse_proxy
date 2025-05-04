d91776a3d19777d9df0a13e8b12d96bc41c5ec86
hedgar bezerra <hedgarbezerra35@gmail.com>
2025-05-04 00:13:51 -0300

    Adição do swagger para o proxy porém sem considerar versões

--- YARPProxy/Program.cs
+++ YARPProxy/Program.cs
@@ -1,25 +1,29 @@
 using Common.Extensions;
 using Common.Middlewares;
 using Common.Policies;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using Yarp.ReverseProxy;
+using YARPProxy.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.UseCustomOpenTelemetry("YARP");
+
+builder.Services.AddSingleton<ISwaggerEndpointManager, SwaggerEndpointManager>();
 builder.Services.AddSingleton(typeof(ITokenService), typeof(TokenService));
 builder.Services.AddRateLimiter(o =>
 {
     o.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
     o.AddPolicy<string,DefaultRateLimitingPolicy>(DefaultRateLimitingPolicy.Name);
 });
-
 builder.Services.AddSecurityServices();
-
+builder.Services.AddHttpClient();  
 builder.Services
     .AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(CorsDefaultPolicy.Name, CorsDefaultPolicy.CorsPolicy);
@@ -28,7 +32,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-app.UseCors();
+app.UseCors(CorsDefaultPolicy.Name);
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
@@ -47,5 +51,63 @@ app.MapReverseProxy(proxyPipeline =>
 });
 app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.UseSerilogRequestLogging();
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    var stateLookup = app.Services.GetRequiredService<IProxyStateLookup>();
+    foreach (var route in stateLookup.GetRoutes().Where(r => !r.Config.RouteId.Contains("poke")))
+    {
+        string title = $"{route.Config.RouteId} - v1";
+        var (key, endpoint) = route.Cluster.Destinations.First();
+        options.SwaggerEndpoint(
+            $"{endpoint.Model.Config.Address}/swagger/v1/swagger.json", title);
+        options.EnableDeepLinking();
+        options.DisplayOperationId();
+        options.DisplayRequestDuration();
+        options.EnableTryItOutByDefault();
+    }
+});
 
 await app.RunAsync();
+
+
+
+// builder.Services.AddSwaggerGen(options =>
+// {
+//     var stateLookup = builder.Services.BuildServiceProvider()
+//         .GetRequiredService<IProxyStateLookup>();
+//
+//     foreach (var route in stateLookup.GetRoutes())
+//     {
+//         options.SwaggerDoc(
+//             description.GroupName,
+//             new OpenApiInfo
+//             {
+//                 Title = $"WebAPI {description.ApiVersion}",
+//                 Version = description.ApiVersion.ToString(),
+//                 Description = description.IsDeprecated ? "Esta versão está depreciada." : string.Empty
+//             });
+//     }
+//     {
+//         options.SwaggerDoc(
+//             description.GroupName,
+//             new OpenApiInfo
+//             {
+//                 Title = $"WebAPI {description.ApiVersion}",
+//                 Version = description.ApiVersion.ToString(),
+//                 Description = description.IsDeprecated ? "Esta versão está depreciada." : string.Empty
+//             });
+//     }
+// });
+
+//TODO: Pode ser necessário adicionar esse trecho quando acessar uma API externa e é importante o cors:
+//app.UseSwagger(c =>
+// {
+//     c.PreSerializeFilters.Add((swagger, httpReq) =>
+//     {
+//         swagger.Servers = new List<OpenApiServer>
+//         {
+//             new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}" }
+//         };
+//     });
+// });
--- webapi01/Program.cs
+++ webapi01/Program.cs
@@ -1,33 +1,68 @@
 using Common;
 using Common.Extensions;
 using Common.Policies;
-using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Serilog;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using Asp.Versioning;
+using Asp.Versioning.ApiExplorer;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.UseSerilog("webapi01");
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(2, 0);
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ReportApiVersions = true;
+        options.ApiVersionReader = ApiVersionReader.Combine(
+            new UrlSegmentApiVersionReader(),
+            new HeaderApiVersionReader("x-api-version"));
+    })
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+    });
 
-builder.Services.AddCors(options =>
+builder.Services.AddSwaggerGen(options =>
 {
-    options.AddPolicy(CorsDefaultPolicy.Name, CorsDefaultPolicy.CorsPolicy);
+    var provider = builder.Services.BuildServiceProvider()
+        .GetRequiredService<IApiVersionDescriptionProvider>();
+
+    foreach (var description in provider.ApiVersionDescriptions)
+    {
+        options.SwaggerDoc(
+            description.GroupName,
+            new OpenApiInfo
+            {
+                Title = $"WebAPI {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = description.IsDeprecated ? "Esta versão está depreciada." : string.Empty
+            });
+    }
 });
+
+builder.Services.AddCors(options => { options.AddPolicy(CorsDefaultPolicy.Name, CorsDefaultPolicy.CorsPolicy); });
 
 var app = builder.Build();
 
+#region V1 - Depreciada
 
-app.MapGet("/hello-world", () => Task.FromResult("Hello world form webapi01"))
+var v1 = app.NewVersionedApi()
+    .MapGroup("/v{version:apiVersion}")
+    .HasApiVersion(new ApiVersion(1, 0));
+
+v1.MapGet("/hello-world", () => Task.FromResult("Hello world form webapi01"))
     .WithName("HelloWorld")
     .WithOpenApi();
 
-app.MapPost("/req", async (HttpContext httpContext) =>
+v1.MapPost("/req", async (HttpContext httpContext) =>
     {
         var requestData = await OpenTelemetryExtensions.GetData(httpContext);
         return Results.Ok(requestData);
@@ -35,48 +70,68 @@ app.MapPost("/req", async (HttpContext httpContext) =>
     .WithName("GetRequestData")
     .WithOpenApi();
 
-
-app.MapPost("/login", (LoginRequest req) =>
-{
-    var tokenHandler = new JwtSecurityTokenHandler();
-    var tokenDescriptor = new SecurityTokenDescriptor
+v1.MapPost("/login", (LoginRequest req) =>
     {
-        Subject = new ClaimsIdentity([
-            new Claim(ClaimTypes.Name, req.Username),
-            new Claim(ClaimTypes.System, Constants.Jwt.Claims.System),
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity([
+                new Claim(ClaimTypes.Name, req.Username),
+                new Claim(ClaimTypes.System, Constants.Jwt.Claims.System),
             ]),
-        Expires = DateTime.UtcNow.AddMinutes(3),
-        NotBefore = DateTime.UtcNow,
-        Issuer = "webapi01",
-        IssuedAt = DateTime.UtcNow,
-        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Constants.Jwt.ApiKey),
-            SecurityAlgorithms.HmacSha256Signature)
-    };
-    var token = tokenHandler.CreateToken(tokenDescriptor);
-    var jwtToken = tokenHandler.WriteToken(token);
+            Expires = DateTime.UtcNow.AddMinutes(3),
+            NotBefore = DateTime.UtcNow,
+            Issuer = "webapi01",
+            IssuedAt = DateTime.UtcNow,
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Constants.Jwt.ApiKey),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var jwtToken = tokenHandler.WriteToken(token);
 
-    return Results.Ok(new { token = jwtToken, expiracy = tokenDescriptor.Expires });
-})
+        return Results.Ok(new { token = jwtToken, expiracy = tokenDescriptor.Expires });
+    })
     .WithName("Login")
     .WithOpenApi();
 
+#endregion
+
+#region V2
+
+var v2 = app.NewVersionedApi()
+    .MapGroup("/v{version:apiVersion}")
+    .HasApiVersion(new ApiVersion(2, 0));
+
+v2.MapGet("/hello-world", () => Results.Ok(new { message = "Hello World from v2" }))
+    .WithName("HelloWorldV2");
+
+#endregion
 
 //Ajusta o caminho da requisição do arquivo openapi.json, necessário para o nginx, YARP e ocelot funcionam sem
-app.UseSwagger(c =>
-{
-    c.PreSerializeFilters.Add((swaggerDoc, httpRequest) =>
-    {
-        if (!httpRequest.Headers.ContainsKey("X-Forwarded-Host"))
-            return;
-        var basePath = "proxy";
-        var serverUrl = $"{httpRequest.Scheme}://{httpRequest.Headers["X-Forwarded-Host"]}/{basePath}";
-        swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = serverUrl } };
-    });
-});
+// app.UseSwagger(c =>
+// {
+//     c.PreSerializeFilters.Add((swaggerDoc, httpRequest) =>
+//     {
+//         if (!httpRequest.Headers.ContainsKey("X-Forwarded-Host"))
+//             return;
+//         var basePath = "proxy";
+//         var serverUrl = $"{httpRequest.Scheme}://{httpRequest.Headers["X-Forwarded-Host"]}/{basePath}";
+//         swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = serverUrl } };
+//     });
+// });
 
-app.UseSwaggerUI();
+app.UseCors(CorsDefaultPolicy.Name);
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    foreach (var description in app.DescribeApiVersions())
+    {
+        options.SwaggerEndpoint(
+            $"/swagger/{description.GroupName}/swagger.json",
+            $"WebAPI {description.GroupName.ToUpperInvariant()}");
+    }
+});
 app.UseSerilogRequestLogging();
-app.UseCors();
 
 await app.RunAsync();
 
